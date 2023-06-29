fe9348fbf7e96c6327717d55a02d0a7bbc1770d0
ismail <109577378+ismaildasci@users.noreply.github.com>
2023-06-29 19:07:39 +0300

    fetching-data-react-query

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import Root from './components/Root';
-import App from './components/App';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 const root = document.getElementById('root');
+const queryClient = new QueryClient();
 createRoot(root).render(
   <React.StrictMode>
-    <Root />
-    {/* <NavigationBar /> */}
+    <QueryClientProvider client={queryClient}>
+      <Root />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </React.StrictMode>
 );
